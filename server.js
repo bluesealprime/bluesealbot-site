b26a5a6e779c2b90c1bc0f4db322add8e79d10c7
bluesealprime <bluesealprime@gmail.com>
2026-03-29 21:04:14 +0530

    Update frontend with Railway absolute URL and add CORS support to backend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const cors = require("cors")
 const axios = require("axios")
 const path = require("path")
 const fs = require("fs")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
 
